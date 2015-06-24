773f36c1ac9f94556a13ec06c89c245620258c12
Dan Büschlen <danbueschlen@gmail.com>
2015-06-24 21:20:11 +0200

    implemented basic structure of the game

--- app/js/game.js
+++ app/js/game.js
@@ -3,21 +3,77 @@
 (function(window){
   var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+  ////////////////////////
+  //  List View
+  ////////////////////////
+
+  var List = function(options) {
+    this.objects = [];
+    this.model = options.model;
+    this.emit('reset');
+  };
+
+  // add emitter events to List class
+  Emitter(List.prototype);
+
+  List.prototype.create = function(data) {
+    this.objects.push(new this.model(data));
+    this.emit('add');
+  };
+
+  ////////////////////////
+  //  List View
+  ////////////////////////
+
+  var ListView = function() {
+
+  };
+
+  ListView.prototype.render = function() {
+    var self = this;
+
+    this.$el.html('');
+
+    this.list.objects.forEach(function(object, i){
+      self.$el.append(object.render());
+    });
+
+    return this.$el;
+  };
+  
+
+
+
+  ////////////////////////
+  //  Game
+  ////////////////////////
+
   var Game = function (options) {
     var self = this;
 
     this.options = $.extend({
       $el: $('#game'),
       currentMonth: 0,
-      families: [],
-      stocks: [],
-      views: [FamilyListView]
     }, options);
 
     this.$el = this.options.$el;
     this.currentMonth = this.options.currentMonth;
-    this.families = this.options.families;
-    this.stocks = this.options.stocks;
+
+    this.lists = {
+      families: new List({
+        model: Family
+      }),
+
+      stocks: new List({
+        model: Stock
+      }),
+
+    };
+
+    this.views = {
+      FamiliesView: new FamilyListView({list: this.lists.families}),
+      StocksView: new StockListView({list: this.lists.stocks})
+    };
 
 
     this.$el.on('submit', '#add-family', function(e){
@@ -40,44 +96,64 @@
       e.target.name.value = '';
     });
 
+    this.on('reset', function(e) {
+      console.log('reset');
+      console.log(e.data);
+    });
+
   };
 
-  // test comment
+  Emitter(Game.prototype);
+
 
   Game.prototype.addFamily = function(data) {
-    this.families.push(new Family({
+    this.lists.families.create({
       name: data.name
-    }));
+    });
   };
 
   Game.prototype.addStock = function(data) {
-    this.stocks.push(new Family({
+    this.lists.stocks.create({
       name: data.name
-    }));
+    });
   };
 
-  Game.prototype.showFamilyListView = function() {
-    // body...
+  Game.prototype.triggerEvent = function() {
+    this.emit('reset', {data: "data"});
   };
 
 
-  var FamilyListView = {
-    $el: null,
-    tagName: 'ul',
-    objectList: null,
-    render: function(){
-      var self = this;
+  ////////////////////////
+  //  Game Initialize View
+  ////////////////////////
 
-      if(this.$el === null) {
-        this.$el = $('<'+this.tagName+'>');
-      }
+  // GameInitializeView = function() {
+  //   var templateSource = $('#game-initialize-template').html();
+  //   this.template = Handlebars.compile(templateSource);
+  // };
 
-      this.objectList.forEach(function(){
-        self.$el.append(this.render())
-      });
-    }
-  };
+  // GameInitializeView.prototype.render = function() {
+  //   this.template({})
+  // };
 
+  // ////////////////////////
+  // //  Game  View
+  // ////////////////////////
+
+  // GameInitializeView = function() {
+  //   var templateSource = $('#game-initialize-template').html();
+  //   this.template = Handlebars.compile(templateSource);
+  // };
+
+  // GameInitializeView.prototype.render = function() {
+  //   this.template({})
+  // };
+
+
+
+  ////////////////////////
+  //  Family
+  ////////////////////////
 
 
   var Family = function(options) {
@@ -100,8 +176,36 @@
   };
 
   Family.prototype.render = function() {
-    return $('<'+this.tagName+' class="family">'+this.name+'</li>')
+    return $('<'+this.tagName+' class="family">'+this.name+'</li>');
   };
+
+
+  var FamilyListView = function(options){
+    var self = this;
+
+    this.list = options.list
+    this.$el = $("#familyList");
+
+    // render the view when the list is reset or has added a new object
+    this.list.on('reset', function(){
+      self.render();
+    });
+
+    this.list.on('add', function(){
+      self.render();
+    });
+  };
+
+  // inherit from ListView class.
+  FamilyListView.prototype = Object.create(ListView.prototype);
+
+
+
+
+  ////////////////////////
+  //  Stock
+  ////////////////////////
+
 
   var Stock = function(options) {
   
@@ -110,7 +214,8 @@
       name: '',
       shareAmount: 100,
       pricePerShare: 1,
-      stakeholders: []
+      stakeholders: [],
+      tagName: 'li'
     }, options);
 
     this.id = this.options.id;
@@ -118,10 +223,44 @@
     this.shareAmount = this.options.shareAmount;
     this.pricePerShare = this.options.pricePerShare;
     this.stakeholders = this.options.stakeholders;
+    this.tagName = this.options.tagName;
     
   };
 
+  Stock.prototype.render = function() {
+    return $('<'+this.tagName+' class="stock">'+this.name+'</li>');
+  };
+
+
+  ////////////////////////
+  //  Stock List View
+  ////////////////////////
+
+
+  var StockListView = function(options){
+    var self = this;
+
+    this.list = options.list
+    this.$el = $('#stockList');
+
+    // render the view when the list is reset or has added a new object
+    this.list.on('reset', function(){
+      self.render();
+    });
+
+    this.list.on('add', function(){
+      self.render();
+    });
+  };
+
+  // inherit from ListView class.
+  StockListView.prototype = Object.create(ListView.prototype);
+
+
+
+  // initialize game
   var game = new Game();
+
 
 
   window.game = game;
